afc6dfc1fe8a22d7e101fa91ffda573b4dbdd7ad
cesarhdz <mrshortcuts@gmail.com>
2015-03-14 11:47:42 -0600

    estatico serve command added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+var
+path = require('path')
 
 
 function App(baseDir, env){
@@ -13,8 +15,99 @@ function App(baseDir, env){
 
 App.DEFAULT_ENVIRONMENT = 'dev'
 
-App.prototype.bootstrap = function(dir){
+// Dependencies can be mocked
+App.Metalsmith = require('metalsmith')
 
+App.prototype.fs = require('fs')
+App.prototype.log = console.log
+
+
+App.Convention = {
+	content: 'content',
+	destination: 'target/work'
+}
+
+
+/**
+ * Validate working dirs has required dirs
+ * 		
+ * @return {Object} Validation object
+ * @param {Boolean} [valid] Whether dir is valid or not
+ * @param {Array} [errors] Errors found in validation
+ */
+App.prototype.validateBaseDir = function(){
+
+	var 
+	self = this,
+
+	// Content and destination folder must exists
+	errors = ['content'].reduce(function(e, key){
+
+		var dir = self.dir(key)
+
+		if(! self.fs.existsSync(dir)){
+			e.push('['+ key +'] dir is missing, it must be located at [' + App.Convention[key] + ']') 
+		}
+
+		return e
+
+	}, [])
+
+
+	return {
+		valid: errors.length === 0,
+		errors: errors 
+	}
+}
+
+/**
+ * Return full qualified dir
+ * 
+ * @param  {String} file 
+ * @return {String}      path
+ */
+App.prototype.dir = function(file){
+	return path.join(this.getBaseDir(), App.Convention[file])
+}
+
+
+/**
+ * Build app using middleware
+ * 
+ * @return {void}
+ */
+App.prototype.build = function(){
+
+	var 
+	self = this,
+	validation = this.validateBaseDir()
+
+	if(!validation.valid){
+		console.warn(validation.errors.join('\n'))
+		throw new Error('Working dir is not valid')
+	}
+
+	var metalsmith = new App.Metalsmith(this.getBaseDir())
+
+	metalsmith.source(App.Convention.content)
+	metalsmith.destination(App.Convention.destination)
+
+	//@TODO Load plugins
+
+	metalsmith.build(function(err){
+	  if (err) return fatal(err.message, err.stack);
+	  self.log('successfully built to ' + metalsmith.destination());
+	})
+
+}
+
+
+/**
+ * Serve files
+ */
+App.prototype.serve = function(){
+
+	this.log('serving')
 
 }
 
