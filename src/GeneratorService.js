53c06c3b76d1030060f6d3473912b13b20285240
cesarhdz <mrshortcuts@gmail.com>
2015-03-16 10:54:11 -0600

    Collection and slug added to pages

--- src/GeneratorService.js
+++ src/GeneratorService.js
@@ -1,6 +1,11 @@
 var
 Metalsmith = require('metalsmith'),
-markdown = require('metalsmith-markdown')
+markdown = require('metalsmith-markdown'),
+collections = require('metalsmith-collections'),
+path = require('path'),
+
+
+DEFAULT_COLLECTION = 'root'
 
 
 function GeneratorService(){}
@@ -31,10 +36,41 @@ GeneratorService.prototype.addPlugins = function(generator, config){
  * @param {Metalsmith} generator 
  * @param {Object} config    
  */
-GeneratorService.prototype.addParser = function(generator, config){
+GeneratorService.prototype.addParser = function(generator, site){
+
+	var self = this
+
+	generator.use(function(files, metal, next){
+		Object.keys(files).forEach(function(page){
+			// {collection}/{name}.{ext}
+			files[page].slug = self.getSlug(page)
+			files[page].collection = self.getCollection(page, files[page].slug)
+		})
+		next()
+	})
+
+	generator.use(collections(site.config.collections))
 	generator.use(markdown())
 }
 
+
+GeneratorService.prototype.getSlug = function(file){
+	var base = path.basename(file),
+	ext = path.extname(base)
+
+	return path.basename(base, ext)
+}
+
+
+GeneratorService.prototype.getCollection = function(file, uri){
+	var separator = '/',
+	parts = file.split(separator)
+
+
+	if(parts.length > 1 && uri !== 'index'){
+	 	return parts[0]
+	}
+}
 
 
 module.exports = GeneratorService
